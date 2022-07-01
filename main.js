54f5517828777d1840c52c50d2d5927f709b2202
vijaydev133 <vijaydeveloper004@gmail.com>
2022-07-01 15:04:43 +0530

    Modifying Code.

---
+++ main.js
@@ -0,0 +1,47 @@
+//INTERVIEW QUESTIONS
+
+
+/******************************************************************** */
+//1. Semantic tags
+//Ans: Semantic tags will have meaning in its name, 
+//which can be used, ex:header,main,aside,footer 
+
+/******************************************************************** */
+// 2. Iframe
+//Ans: Iframe can be used to embed another html page inside the html page.
+
+/******************************************************************** */
+// 3. Z index
+//Ans: Z index is a css property which can be used in position and display.
+
+/******************************************************************** */
+// 4. Canvas tag
+//Ans: Canvas tag is used to draw graphics usually with the javascript
+
+/******************************************************************** */
+// 5. Hosting
+//Ans: Hoisting is moving(not physically) the function and variable declaration
+//to the top of the javascript
+
+/******************************************************************** */
+// 6. let,var,const difference
+//let is a block scope, where reassigning of that variable without let
+//keyword is possible
+
+//var is function scope, where we can reaasign the variable with the var 
+//keyword is possible
+
+//const is a block scope, where reassigning is not possible, it should be
+//declared along with the value initialization.
+
+/******************************************************************** */
+// 7. Position
+//Ans: position is a css property, which is used to position the element 
+//according to our needs
+//types of position
+//static(default)
+//relative
+//absolute
+//sticky
+//fixed
+/******************************************************************** */
